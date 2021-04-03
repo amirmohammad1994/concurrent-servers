4bd922a5a7ac537a251ae63911104945c379a271
amirmohammadohadi <amirmohammadohadi@gmail.com>
2021-04-03 17:28:30 +0430

    first try.

---
+++ include/utils.h
@@ -0,0 +1,10 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+// void die
+
+#endif // __UTILS_H__
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// In the Name of Allah
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "hello world!";
+  return 0;
+}
